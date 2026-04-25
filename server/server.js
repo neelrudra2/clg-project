d238c75acbf005c1c39f4a1f77f4189e2a7e9c35
neelrudra2 <neel.rudra.mailbox@gmail.com>
2026-04-25 12:25:20 +0530

    new commits

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://your-frontend.vercel.app'],
+    origin: ['http://localhost:5173', 'https://nitaplacementpro.vercel.app'],
     credentials: true,
   })
 );
